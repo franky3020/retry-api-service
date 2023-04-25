91f9d6a3cd21ad48c73f50481ad68fce7860c8e0
frankyya <frankyya@hitrust.com.tw>
2023-04-25 11:52:05 +0800

    新增測試

---
+++ src/app/platinum-api.service.spec.ts
@@ -0,0 +1,82 @@
+import { TestBed } from '@angular/core/testing';
+
+import {PlatinumApiService, TokenInValidError} from './platinum-api.service';
+
+describe('PlatinumApiService', () => {
+  let service: PlatinumApiService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(PlatinumApiService);
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('when token empty then token is invalid', () => {
+    service.setToken('');
+    let isTokenValid= service.isTokenValid();
+    expect(isTokenValid).toBe(false);
+  });
+
+  it('when token empty then token is invalid', () => {
+    service.setToken('');
+    let isTokenValid= service.isTokenValid();
+    expect(isTokenValid).toBe(false);
+  });
+
+  it('when token is expired then token is invalid', () => {
+    service.setToken('sdfasd');
+    service.isTokenExpired = true;
+
+    let isTokenValid= service.isTokenValid();
+    expect(isTokenValid).toBe(false);
+  });
+
+  it('when token is not expired and has value then token is valid', () => {
+    service.setToken('sdfasd');
+    service.isTokenExpired = false;
+
+    let isTokenValid= service.isTokenValid();
+    expect(isTokenValid).toBe(true);
+  });
+
+  it('when platinum api throw invalid error', async () => {
+
+    let service: PlatinumApiService;
+    service = TestBed.inject(PlatinumApiService);
+    service.isTokenValid = () => {
+      return false;
+    }
+    await expectAsync(service.sendApi()).toBeRejectedWith(new TokenInValidError());
+  });
+
+  it('when platinum api success', async () => {
+
+    let service: PlatinumApiService;
+    service = TestBed.inject(PlatinumApiService);
+    service.isTokenValid = () => {
+      return true;
+    }
+
+    let res = await service.sendApi();
+    expect(res).toBe("{mockJson:''}");
+  });
+
+  it('when platinum token expired will retry send api one time', async () => {
+
+    // let service: PlatinumApiService;
+    // service = TestBed.inject(PlatinumApiService);
+    // service.isTokenExpired = true;
+    //
+    // // spy 它 sendApiToPlatinum
+    //
+    //
+    // let res = await service.sendApi();
+    // expect(res).toBe("{mockJson:''}");
+  });
+
+
+});
---
+++ src/app/platinum-api.service.ts
@@ -0,0 +1,126 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlatinumApiService {
+
+
+  token = '';
+
+  isTokenExpired = true;
+
+  constructor() { }
+
+  setToken(token: string) {
+    this.token = token;
+  }
+
+  /**
+   * 處理 發送 API 時的流程:
+   * . 當發送後的 res 回應成功, 則回傳成功的 req
+   * . 當遇到 token 無效, 則要先取得 token
+   * . 當發送後的 res 回應 token 已經過期, 則重新拿取 token, 且再次發送API
+   * . 當發送後的 res 回應  token 無效, 則拋出例外
+   * . 當發送後的 res 無回應, 則拋出例外
+   * . 當發送後的 res 回應其他錯誤, 則拋出例外
+   */
+  async sendApi() {
+
+    const apiMaxSendTime = 2;
+    let apiSendTime = 0;
+
+    let res = '';
+
+    let error: Error = new Error('unknown Error');
+
+    while(apiSendTime < apiMaxSendTime) {
+      try {
+        res = await this.sendApiToPlatinum();
+        return res;
+      } catch (err: any) {
+        error = err;
+        if (err instanceof TokenExpiredError) {
+          // to retry
+        } else {
+          throw err;
+        }
+
+      } finally {
+        apiSendTime++;
+      }
+    }
+    throw error;
+  }
+
+  /**
+   * 可能拋出 [token 過期, token 無效, 無回應]的例外
+   */
+  async sendApiToPlatinum(): Promise<string> {
+
+    if (!this.isTokenValid()) {
+      await this.resetTokenFromAPI();
+    }
+
+    if (!this.isTokenValid()) {
+      throw new TokenInValidError();
+    }
+
+    //  發送API到廠商那 有可能 throw 過期 TokenExpiredError
+
+    return "{mockJson:''}";
+  }
+
+  /**
+   * 跟 FBO 中台拿 token, 且重設 token
+   */
+  async resetTokenFromAPI() {
+    let token = await this.mockGetTokenFromFBO();
+    this.setToken(token);
+    this.isTokenExpired = true;
+  }
+
+  /**
+   * if not expired or has token
+   */
+  isTokenValid() {
+    if (this.token === '' || this.isTokenExpired) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+
+  mockGetTokenFromFBO(): Promise<string> {
+    return new Promise(async (resolve) => {
+      await sleep(1000);
+      return resolve('token_from_fbo');
+    });
+  }
+
+}
+
+
+export class TokenExpiredError extends Error {
+
+  constructor() {
+    super('TokenExpiredError');
+    Object.setPrototypeOf(this, TokenExpiredError.prototype);
+  }
+
+}
+
+export class TokenInValidError extends Error {
+
+  constructor() {
+    super('TokenInValidError');
+    Object.setPrototypeOf(this, TokenInValidError.prototype);
+  }
+
+}
+
+function sleep(millisecond: number): Promise<void> {
+  return new Promise((resolve) => {setTimeout(()=>{return resolve();}, millisecond)});
+}
